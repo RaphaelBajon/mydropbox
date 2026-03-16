57d657dc924332f044dbc75f1f115427074fdba6
Raphaël Bajon <raphael.bajon@protonmail.com>
2026-03-16 13:51:01 -1000

    change structure fix

--- dropbox/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dropbox_path import get_dropbox, DropboxPaths
-from .personal_path import PersonalPaths
-from .group_path import GroupPaths
-
-__all__ = ["get_dropbox", "DropboxPaths", "GroupPaths", "PersonalPaths",]
--- project/__init__.py
+++
@@ -1,6 +0,0 @@
-from .projects import ProjectPaths, create_project
-
-__all__ = [
-    "create_project",
-    "ProjectPaths",
-]
